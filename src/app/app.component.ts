14ede3a297c1a93a051a41240a096e8f04af49f7
sonnew <92101022+sonnew@users.noreply.github.com>
2021-10-20 11:35:46 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project Wout';
+  title = 'Angular project Wout';
+  data = 'Hallo';
+  num=100;
 }
